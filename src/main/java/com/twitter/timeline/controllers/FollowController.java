493ca13233ec204992bf139865f9033b3db0b7c7
shubham88fru <shubham88fru@yahoo.com>
2025-03-15 15:31:26 -0700

    add skeleton for controllers

---
+++ src/main/java/com/twitter/timeline/controllers/FollowController.java
@@ -0,0 +1,9 @@
+package com.twitter.timeline.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class FollowController {
+}
